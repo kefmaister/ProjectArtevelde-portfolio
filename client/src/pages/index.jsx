f904b14c094befa8404ca42a9adfcbcd79d2411d
kefmaister <kevin.dworschak@student.arteveldehs.be>
2024-06-19 14:33:31 +0200

    Add project

---
+++ client/src/pages/index.jsx
@@ -0,0 +1,20 @@
+import HomePage from "./Home";
+import ProjectsPage from "./Projects";
+import ServicesPage from "./Service";
+import AboutPage from "./About";
+import SkillPage from "./Skills";
+import ServiceDetailPage from "./ServiceDetail";
+
+import SearchResults from "./SearchResults";
+import NotFound from "./NotFound";
+
+export {
+  HomePage,
+  ProjectsPage,
+  ServicesPage,
+  ServiceDetailPage,
+  SkillPage,
+  AboutPage,
+  SearchResults,
+  NotFound,
+};
---
+++ client/src/routes/routes.jsx
@@ -0,0 +1,30 @@
+export const ROUTES = {
+  home: {
+    path: "/",
+    title: "Home",
+  },
+  projects: {
+    path: "/projects",
+    title: "Projects",
+  },
+  skills: {
+    path: "/skills",
+    title: "Knowledge",
+  },
+  services: {
+    path: "/services",
+    title: "Services",
+  },
+  serviceDetail: {
+    path: "/service/:slug",
+    title: "Service Detail",
+  },
+  about: {
+    path: "/about-me",
+    title: "About Me",
+  },
+  searchResults: {
+    path: "/search/:searchTerm",
+    title: "Search Results",
+  },
+};
